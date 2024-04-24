88e20cd5432f47f2437c9e836dd8eb41b9cce3b2
sruthivellore <sruthivellore99@gmail.com>
2024-04-24 05:06:31 -0400

    vertical nav bar update

--- frontend/management/urls.py
+++ frontend/management/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('employee/', views.view_employee, name='view_employee'),
     path('facility/', views.view_facility, name='view_facility'),
-    path('insurance/', views.view_insurance, name='view_insurance'),
+    path('insurance/', views.view_insurance, name='view_insurance')
 ]
