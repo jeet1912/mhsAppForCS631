1bc089d16540eb185cebf041755f970ead0f2b02
sruthivellore <sruthivellore99@gmail.com>
2024-04-24 20:09:17 -0400

    Add employee implementes.

--- frontend/management/urls.py
+++ frontend/management/urls.py
@@ -3,14 +3,14 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('employee/', views.view_employee, name='view_employee'),
     path('facility/', views.view_facility, name='view_facility'),
     path('insurance/', views.view_insurance, name='view_insurance'),
     path('patient/', views.view_patient, name='view_patient'),
     path('report/', views.view_report, name='view_report'),
-    path('doctor/', views.view_doctor, name='view_doctor'),
-    path('nurse/', views.view_nurse, name='view_nurse'),
-    path('admin_staff/', views.view_admin_staff, name='view_admin_staff'),
-    path('hcp/', views.view_hcp, name='view_hcp'),
-    path('employee_assignment/', views.view_employee_assignment, name='view_employee_assignment'),
+    path('emp/doctor/', views.view_doctor, name='view_doctor'),
+    path('emp/nurse/', views.view_nurse, name='view_nurse'),
+    path('emp/admin_staff/', views.view_admin_staff, name='view_admin_staff'),
+    path('emp/hcp/', views.view_hcp, name='view_hcp'),
+    path('emp/add_employee/', views.add_employee, name='add_employee'),
+    path('emp/', views.view_employee, name='view_employee'),
 ]
--- frontend/management/views.py
+++ frontend/management/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from .db_utils import execute_query
 from django.core.paginator import Paginator
+from django.http import HttpResponse
 
 def index(request):
     return render(request, 'intro.html')
 
+# Employee Management Views
 def view_employee(request):
     sql = "SELECT * FROM EMPLOYEE"
     employees = execute_query(sql, fetchall=True)
@@ -13,26 +15,7 @@ def view_employee(request):
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
     
-    return render(request, 'employee.html', {'employees': page_obj})
-
-def view_facility(request):
-    sql = "SELECT * FROM FACILITY"
-    facilities = execute_query(sql, fetchall=True)
-    return render(request, 'facility.html', {'facilities': facilities})
-
-def view_insurance(request):
-    sql = "SELECT * FROM INSURANCE_COMPANY"
-    insurance_companies = execute_query(sql, fetchall=True)
-    return render(request, 'insurance.html', {'insurance_companies': insurance_companies})
-
-def view_patient(request):
-    sql = "SELECT * FROM PATIENT"
-    patients = execute_query(sql, fetchall=True)
-    return render(request, 'patient.html', {'patients': patients})
-
-def view_report(request):
-    # Logic for generating reports
-    return render(request, 'report.html')
+    return render(request, 'employee/employee.html', {'employees': page_obj})
 
 def view_doctor(request):
     base_sql = """
@@ -55,38 +38,181 @@ def view_doctor(request):
             EMPLOYEE E ON D.EmployeeID = E.EmployeeID
     """
 
-    page_number = request.GET.get('page')
+    doctors = execute_query(base_sql, fetchall=True)
     
-    per_page = 2
-    offset = (int(page_number) - 1) * per_page if page_number else 0
-    paginate_sql = f"{base_sql} LIMIT {per_page} OFFSET {offset}"
-    doctors = execute_query(paginate_sql, fetchall=True)
+    paginator = Paginator(doctors, 5)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
 
-    count_sql = f"SELECT COUNT(*) AS COUNT FROM ({base_sql}) AS count_query"
-    total_doctors_count = execute_query(count_sql, fetchone=True)['COUNT']
-    total_pages = (total_doctors_count + per_page - 1) // per_page
-
-    return render(request, 'doctor.html', {'doctors': doctors, 'total_pages': total_pages})
+    return render(request, 'employee/doctor.html', {'doctors': page_obj})
 
 
 def view_nurse(request):
-    sql = "SELECT * FROM NURSE"
-    nurses = execute_query(sql, fetchall=True)
-    return render(request, 'nurse.html', {'nurses': nurses})
+    base_sql = """
+        SELECT 
+            N.*,
+            E.FirstName AS EmpFirstName,
+            E.MiddleName AS EmpMiddleName,
+            E.LastName AS EmpLastName,
+            E.Street AS EmpStreet,
+            E.City AS EmpCity,
+            E.State AS EmpState,
+            E.Zip AS EmpZip,
+            E.Salary AS EmpSalary,
+            E.Date_Hired AS EmpDateHired,
+            E.SSN AS SSN,
+            E.Fac_ID AS EmpFacilityID
+        FROM 
+            NURSE N
+        INNER JOIN 
+            EMPLOYEE E ON N.EmployeeID = E.EmployeeID
+    """
+
+    nurses = execute_query(base_sql, fetchall=True)
+    
+    paginator = Paginator(nurses, 5)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
+    return render(request, 'employee/nurse.html', {'nurses': page_obj})
 
 def view_admin_staff(request):
-    sql = "SELECT * FROM ADMIN_STAFF"
-    admin_staff = execute_query(sql, fetchall=True)
-    return render(request, 'admin_staff.html', {'admin_staff': admin_staff})
+    base_sql = """
+        SELECT 
+            A.*,
+            E.FirstName AS EmpFirstName,
+            E.MiddleName AS EmpMiddleName,
+            E.LastName AS EmpLastName,
+            E.Street AS EmpStreet,
+            E.City AS EmpCity,
+            E.State AS EmpState,
+            E.Zip AS EmpZip,
+            E.Salary AS EmpSalary,
+            E.Date_Hired AS EmpDateHired,
+            E.SSN AS SSN,
+            E.Fac_ID AS EmpFacilityID
+        FROM 
+            ADMIN_STAFF A
+        INNER JOIN 
+            EMPLOYEE E ON A.EmployeeID = E.EmployeeID
+    """
+    admin_staff = execute_query(base_sql, fetchall=True)
+    
+    paginator = Paginator(admin_staff, 5)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
+    return render(request, 'employee/admin_staff.html', {'admin_staff': page_obj})
 
 def view_hcp(request):
-    sql = "SELECT * FROM OTHER_HCP"
-    other_hcp = execute_query(sql, fetchall=True)
-    return render(request, 'hcp.html', {'other_hcp': other_hcp})
+    base_sql = """
+        SELECT 
+            H.*,
+            E.FirstName AS EmpFirstName,
+            E.MiddleName AS EmpMiddleName,
+            E.LastName AS EmpLastName,
+            E.Street AS EmpStreet,
+            E.City AS EmpCity,
+            E.State AS EmpState,
+            E.Zip AS EmpZip,
+            E.Salary AS EmpSalary,
+            E.Date_Hired AS EmpDateHired,
+            E.SSN AS SSN,
+            E.Fac_ID AS EmpFacilityID
+        FROM 
+            OTHER_HCP H
+        INNER JOIN 
+            EMPLOYEE E ON H.EmployeeID = E.EmployeeID
+    """
+    hcps = execute_query(base_sql, fetchall=True)
+    
+    paginator = Paginator(hcps, 5)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
 
-def view_employee_assignment(request):
-    # Depending on your specific logic for employee assignments, you can fetch data from appropriate tables
-    # For example:
-    sql = "SELECT * FROM EMPLOYEE_ASSIGNMENT"
-    employee_assignments = execute_query(sql, fetchall=True)
-    return render(request, 'employee_assignment.html', {'employee_assignments': employee_assignments})
+    return render(request, 'employee/hcp.html', {'other_hcp': page_obj})
+
+def add_employee(request):
+    if request.method == 'POST':
+        ssn = request.POST.get('ssn')
+        first_name = request.POST.get('first_name')
+        middle_name = request.POST.get('middle_name', '')
+        last_name = request.POST.get('last_name')
+        street = request.POST.get('street')
+        city = request.POST.get('city')
+        state = request.POST.get('state')
+        zip_code = request.POST.get('zip')
+        salary = request.POST.get('salary')
+        date_hired = request.POST.get('date_hired')
+        job_class = request.POST.get('job_class')
+        facility_id = request.POST.get('facility_id')
+
+        # Inserting into EMPLOYEE table
+        employee_sql = """
+        INSERT INTO EMPLOYEE (SSN, FirstName, MiddleName, LastName, Street, City, State, Zip, Salary, Date_Hired, Job_Class, Fac_ID)
+        VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+        """
+        employee_params = (ssn, first_name, middle_name, last_name, street, city, state, zip_code, salary, date_hired, job_class, facility_id)
+        execute_query(employee_sql, params=employee_params)
+
+        # Inserting into specific table based on job class
+        if job_class == 'Doctor':
+            speciality = request.POST.get('speciality')
+            board_certification_date = request.POST.get('board_certification_date')
+            doctor_sql = """
+            INSERT INTO DOCTOR (EmployeeID, Speciality, Board_Certification_Date)
+            VALUES (LAST_INSERT_ID(), %s, %s)
+            """
+            doctor_params = (speciality, board_certification_date)
+            execute_query(doctor_sql, params=doctor_params)
+        elif job_class == 'Nurse':
+            certification = request.POST.get('certification')
+            nurse_sql = """
+            INSERT INTO NURSE (EmployeeID, Certification)
+            VALUES (LAST_INSERT_ID(), %s)
+            """
+            nurse_params = (certification,)
+            execute_query(nurse_sql, params=nurse_params)
+        elif job_class == 'HCP':
+            practice_area = request.POST.get('practice_area')
+            hcp_sql = """
+            INSERT INTO OTHER_HCP (EmployeeID, Practice_Area)
+            VALUES (LAST_INSERT_ID(), %s)
+            """
+            hcp_params = (practice_area,)
+            execute_query(hcp_sql, params=hcp_params)
+        elif job_class == 'Admin':
+            job_title = request.POST.get('job_title')
+            admin_sql = """
+            INSERT INTO ADMIN_STAFF (EmployeeID, Job_Title)
+            VALUES (LAST_INSERT_ID(), %s)
+            """
+            admin_params = (job_title,)
+            execute_query(admin_sql, params=admin_params)
+
+        return HttpResponse("Employee added successfully!")
+
+    # Fetching facility IDs for dropdown
+    facility_sql = "SELECT Facility_ID FROM FACILITY"
+    facilities = execute_query(facility_sql, fetchall=True)
+    return render(request, 'employee/add_employee.html', {'facilities': facilities})
+
+
+def view_facility(request):
+    sql = "SELECT * FROM FACILITY"
+    facilities = execute_query(sql, fetchall=True)
+    return render(request, 'facility.html', {'facilities': facilities})
+
+def view_insurance(request):
+    sql = "SELECT * FROM INSURANCE_COMPANY"
+    insurance_companies = execute_query(sql, fetchall=True)
+    return render(request, 'insurance.html', {'insurance_companies': insurance_companies})
+
+def view_patient(request):
+    sql = "SELECT * FROM PATIENT"
+    patients = execute_query(sql, fetchall=True)
+    return render(request, 'patient.html', {'patients': patients})
+
+def view_report(request):
+    # Logic for generating reports
+    return render(request, 'report.html')
